168772df6992c4e51935ccb27b6977dc8f58a446
zirdeli manyak <zirdelimanyak@hotmail.com>
2021-05-12 16:11:30 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=3,ModelYear=2020,DailyPrice=600,Description="VIP müşterilerimiz için"},
+                new Car{CarId=2,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=500,Description="Kendini VIP hissetmek isteyenler için"},
+                new Car{CarId=3,BrandId=2,ColorId=2,ModelYear=2010,DailyPrice=400,Description="Tam bir fiyat performans arabası"},
+                new Car{CarId=4,BrandId=3,ColorId=1,ModelYear=2005,DailyPrice=300,Description="Hem sağlam Hem güçlü"},
+                new Car{CarId=5,BrandId=3,ColorId=4,ModelYear=2000,DailyPrice=200,Description="Az yakar çok kaçar. İşinizi sorunsuz görür"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _cars.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
